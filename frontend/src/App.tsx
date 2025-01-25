52a0585237181a8f23df119c904870eb106ff3fb
Owen Shadburne <91618801+CubeTures@users.noreply.github.com>
2025-01-25 11:15:13 -0600

    Connect with websockets

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,14 @@ function App() {
   const [ws, setWs] = useState<WebSocket | undefined>(undefined);
 
   function connect() {
-    
+    const temp = new WebSocket("ws://10.246.176.24:6969/ws");
+    temp.onopen = () => console.log("Open");
+    temp.onmessage = onMessage;
+    setWs(temp);
+  }
+
+  function onMessage(messageJSON: MessageEvent) {
+    console.log(messageJSON.data);
   }
 
   return (
