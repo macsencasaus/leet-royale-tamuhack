76a15d0fa39f4991469a57ddb1811e0ddbf501d5
Macsen Casaus <macsencasaus@gmail.com>
2025-01-25 11:15:14 -0600

    test websocket connection

--- backend/go.mod
+++ backend/go.mod
@@ -1,3 +1,5 @@
 module leet-guys
 
 go 1.21.6
+
+require github.com/gorilla/websocket v1.5.3
--- backend/main.go
+++ backend/main.go
@@ -1,9 +1,30 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/websocket"
 )
 
 func main() {
-    fmt.Println("hello world!")
+
+	mux := http.NewServeMux()
+
+	var upgrader = websocket.Upgrader{
+		CheckOrigin: func(r *http.Request) bool { return true },
+	}
+
+	mux.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		conn, err := upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			fmt.Println(err)
+			panic("Error creating websocket")
+		}
+
+		conn.WriteMessage(websocket.TextMessage, []byte("Poop fart"))
+	})
+
+	fmt.Println("Serving on 10.246.176.24:6969")
+	http.ListenAndServe("10.246.176.24:6969", mux)
 }
