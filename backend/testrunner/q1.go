03e4c52fef69437c9331fa214ff9b2b6a5ff6f20
jacklewicki2 <jacklewicki@tamu.edu>
2025-01-25 22:17:40 -0600

    preliminary test generation

---
+++ backend/testrunner/q1.go
@@ -0,0 +1,21 @@
+package testrunner
+
+var Q1 = reflectionData{
+	numParams:  2,
+	paramTypes: []string{"int", "int"},
+	methodName: "add",
+	numCases:   5,
+	cases: [][][]string{{{"1"}, {"2"}},
+		{{"-10"}, {"7"}},
+		{{"2147483647"}, {"-2147483648"}},
+		{{"-543"}, {"543"}},
+		{{"-500"}, {"4500"}}},
+	expectedResults: []string{"3", "-3", "-1", "0", "4000"},
+	returnType:      "int",
+}
+
+var Q1Template = map[string]string{
+	"python": `def add(a:int, b:int):`,
+    "c++": `int add(int a, int b){`,
+	"javascript":`func add(a, b){`,
+} 
---
+++ backend/testrunner/q3.go
@@ -0,0 +1,21 @@
+package testrunner
+
+var Q3 = reflectionData{
+	numParams:  1,
+	paramTypes: []string{"list int"},
+	methodName: "returnList",
+	numCases:   5,
+	cases: [][][]string{{{"1","2"}},
+		{{"-10"}},
+		{{"2147483647","-2147483648","1515125"}},
+		{{}},
+		{{"-500","4500"}}},
+	expectedResults: []string{"2","1","2","1","-10","3","2147483647","-2147483648","1515125","0","2","-500","4500"},
+	returnType:      "list int",
+}
+
+var Q3Template = map[string]string{
+	"python": `def returnList(ls:list):`,
+    "c++": `vector<int> returnList(vector<int> list){`,
+	"javascript":`func returnList(ls){`,
+} 
